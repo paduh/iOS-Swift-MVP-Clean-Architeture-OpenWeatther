cde6d5de01eab060ab5ff418842cf761592f632e
Perfect Aduh <paduh@getcarcon.co>
2021-06-08 03:00:24 +0100

    implemented a generic and reusable GenericTableViewDataSourceDelegate

---
+++ DVT-Weather/GenericTableViewDataSourceDelegate/GenericTableViewDataSourceDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  GenericTableViewDataSourceDelegate.swift
+//  DVT-Weather
+//
+//  Created by Aduh Perfect on 07/06/2021.
+//
+
+import UIKit
+
+// MARK: - GenericTableViewDataSourceDelegate
+
+class GenericTableViewDataSourceDelegate<Model, Cell: UITableViewCell>: NSObject, UITabBarDelegate, UITableViewDataSource {
+    
+    // MARK: Properties
+    
+    typealias CellConfigurator = ((Model, Cell) -> Void)
+    var models: [Model]
+    private let cellConfigurator: CellConfigurator
+    
+    // MARK: Initializer
+    
+    init(models: [Model], cellConfigurator: @escaping CellConfigurator) {
+        self.models = models
+        self.cellConfigurator = cellConfigurator
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeue(Cell.self, for: indexPath)
+        cellConfigurator(model, cell)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+}
